d80c5c62694b81dc932ae5849d6b93efa667762d
CyberCodeLab <cybercodelab@ik.me>
2022-03-22 17:40:39 +0100

    Add zombieStates.lua

---
+++ zombieStates.lua
@@ -0,0 +1,10 @@
+local ZOMBIES_STATES = {}
+
+ZOMBIES_STATES = {}
+ZOMBIES_STATES.NONE = ""
+ZOMBIES_STATES.WALK = "walk" -- it walking around
+ZOMBIES_STATES.ATTACK = "track" -- it tracking the target
+ZOMBIES_STATES.BITE = "attack" -- it bite its targets
+ZOMBIES_STATES.CHANGE_DIRRECTION = "change" -- it follow a random path
+
+return ZOMBIES_STATES
