6e8297b0f214b84529d5bcc8bbe53ab9fe950322
CyberCodeLab <cybercodelab@ik.me>
2022-03-23 19:10:03 +0100

    Up zombieStates.lua

--- zombieStates.lua
+++ zombieStates.lua
@@ -7,13 +7,12 @@
 ╚══════╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝     ╚═╝    ╚══════╝   ╚═╝   ╚═╝  ╚═╝   ╚═╝   ╚══════╝╚══════╝
 ]]
 
-local ZOMBIES_STATES = {}
-
-ZOMBIES_STATES = {}
-ZOMBIES_STATES.NONE = ""
-ZOMBIES_STATES.WALK = "walk" -- it walking around
-ZOMBIES_STATES.ATTACK = "track" -- it tracking the target
-ZOMBIES_STATES.BITE = "attack" -- it bite its targets
-ZOMBIES_STATES.CHANGE_DIRRECTION = "change" -- it follow a random path
+local ZOMBIES_STATES = {
+    NONE = "",
+    WALK = "walk", -- it walking around
+    ATTACK = "track", -- it tracking the target
+    BITE = "attack", -- it bite its targets
+    CHANGE_DIRRECTION = "change" -- it follow a random path
+}
 
 return ZOMBIES_STATES
